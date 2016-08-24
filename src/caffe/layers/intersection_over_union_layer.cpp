9cf309d10d62d2de8330c0df0c26f57d37541834
Yifei Li <liyifei.leo@gmail.com>
2016-08-24 02:27:25 -0500

    lll

--- src/caffe/layers/intersection_over_union_layer.cpp
+++ src/caffe/layers/intersection_over_union_layer.cpp
@@ -83,23 +83,19 @@ void IntersectionOverUnionLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& 
 		//calculate P_i
 		//predicting class_idx, ground truth in all class
 		for(int n = 0; n < num;n++){
-			for(int i =0; i< classes;i++){
 				for(int h = 0; h < height; h++){
 					for(int w = 0; w < width; w++){
-						const int pred_idx = ((n * classes + i) * height + h) * width + w;
+						const int pred_idx = ((n * classes + class_idx) * height + h) * width + w;
 						const int label_idx = (n * height + h) * width + w;
 						std::cout << "pred_idx: "<< pred_idx << std::endl;
 							std::cout << "bottom_data: " << bottom_data[pred_idx]<< std::endl;
-							if(bottom_data[pred_idx]==1){
-								if(bottom_label[label_idx] == class_idx){
+							if(bottom_data[pred_idx]==1 && bottom_label[label_idx] == class_idx){
 									P_i++;
 									std::cout << "TRUE" << std::endl;
 							}
-						}
 					}
 				}
 				std::cout<<std::endl;
-			}
 		}
 		//calculate IU for each class
 		std::cout << "C_i:"<< C_i <<std::endl;
